893549cefe51926ba610470ea8d0209bed0d9272
joaomendoncaa <joaomendoncadev@gmail.com>
2024-03-24 02:39:33 +0000

    feat: bash conform, changes to init

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -606,7 +606,7 @@ require('lazy').setup({
     end,
   },
 
-  { -- Autoformat
+  {
     'stevearc/conform.nvim',
     opts = {
       notify_on_error = false,
@@ -622,11 +622,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use a sub-list to tell conform to run *until* a formatter
-        -- is found.
+        sh = { 'shfmt' },
         javascript = { { 'prettierd', 'prettier' } },
       },
     },
--- init.sh
+++ init.sh
@@ -2,87 +2,91 @@
 set -euo pipefail
 
 if [[ ${OS:-} = Windows_NT ]]; then
-    echo 'error: This config is destined to a UNIX based system.'
-    exit 1
+	echo 'error: This config is destined to a UNIX based system.'
+	exit 1
 fi
 
 if [[ -t 1 ]]; then
-    # Reset
-    Color_Off='\033[0m' # Text Reset
+	# Reset
+	Color_Off='\033[0m' # Text Reset
 
-    # Regular Colors
-    Red='\033[0;31m'   # Red
-    Green='\033[0;32m' # Green
-    Dim='\033[0;2m'    # White
+	# Regular Colors
+	Red='\033[0;31m'   # Red
+	Green='\033[0;32m' # Green
+	Dim='\033[0;2m'    # White
 
-    # Bold
-    Bold_Green='\033[1;32m' # Bold Green
-    Bold_White='\033[1m'    # Bold White
+	# Bold
+	Bold_Green='\033[1;32m' # Bold Green
+	Bold_White='\033[1m'    # Bold White
 fi
 
+# Globals
 force_delete=false
 
+# Check args
 while [[ $# -gt 0 ]]; do
-    case "$1" in
-        --force)
-            force_delete=true
-            ;;
-        *)
-            echo "Unknown option: $1"
-            exit 1
-            ;;
-    esac
-    shift
+	case "$1" in
+	--force)
+		force_delete=true
+		;;
+	*)
+		echo "Unknown option: $1"
+		exit 1
+		;;
+	esac
+	shift
 done
 
 error() {
-    echo -e "${Red}error${Color_Off}:" "$@" >&2
-    exit 1
+	echo -e "${Red}error${Color_Off}:" "$@" >&2
+	exit 1
 }
 
 error_silent() {
-    echo -e "${Red}error${Color_Off}:" "$@" >&2
+	echo -e "${Red}error${Color_Off}:" "$@" >&2
 }
 
 info() {
-    echo -e "${Dim}$@ ${Color_Off}"
+	echo -e "${Dim}$@ ${Color_Off}"
 }
 
 info_bold() {
-    echo -e "${Bold_White}$@ ${Color_Off}"
+	echo -e "${Bold_White}$@ ${Color_Off}"
 }
 
 success() {
-    echo -e "${Green}$@ ${Color_Off}"
+	echo -e "${Green}$@ ${Color_Off}"
 }
 
 # Simplify creating a symlink with this repository /dotfiles/*
 # TODO: interactive mode to configure paths on the fly (right now it's only an interactive "deletion")
 create_symlink() {
-  local config_path="$1"
-  local source_path="$2"
+	local config_path="$1"
+	local source_path="$2"
 
- if [[ "$force_delete" == true || ! -e "$config_path" ]]; then
-    rm -rf "$config_path"
-    ln -s "$source_path" "$config_path"
-    success "Symlink created successfully: $source_path -> $config_path"
-  else
-    error_silent "There's already a config in $config_path"
-    info "You can delete it with rm -rf $config_path"
-    
-    read -p "Do you want to delete it now and proceed with the symlinking? [Y/N] " response
-    response=${response^^}  # convert response to uppercase
-    if [[ $response =~ ^(YES|Y)$ ]]; then
-        rm -rf "$config_path"
-        ln -s "$source_path" "$config_path"
-        success "Symlink created successfully: $source_path -> $config_path"
-    else
-        info "Symlinking aborted."
-    fi
-  fi
+	if [[ "$force_delete" == true || ! -e "$config_path" ]]; then
+		rm -rf "$config_path"
+		ln -s "$source_path" "$config_path"
+
+		success "Symlink created successfully: $source_path -> $config_path"
+	else
+		error_silent "There's already a config in $config_path"
+		info "You can delete it with rm -rf $config_path"
+
+		read -p "Do you want to delete it now and proceed with the symlinking? [Y/N] " response
+		response=${response^^} # convert response to uppercase
+		if [[ $response =~ ^(YES|Y)$ ]]; then
+			rm -rf "$config_path"
+			ln -s "$source_path" "$config_path"
+
+			success "Symlink created successfully: $source_path -> $config_path"
+		else
+			info "Symlinking aborted."
+		fi
+	fi
 }
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 create_symlink "/home/joao/.config/nvim" "$DIR/dotfiles/nvim"
 create_symlink "/home/joao/.config/starship.toml" "$DIR/dotfiles/starship/starship.toml"
