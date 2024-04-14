eabe5b61176e678956a09db4696871f895afcf8c
joaomendoncaa <joaomendoncadev@gmail.com>
2024-04-14 18:40:54 +0100

    fix: lazygit tweaks

--- dotfiles/nvim/lua/custom/plugins/lazygit.lua
+++ dotfiles/nvim/lua/custom/plugins/lazygit.lua
@@ -1,6 +1,7 @@
 return {
   {
     'kdheepak/lazygit.nvim',
+
     cmd = {
       'LazyGit',
       'LazyGitConfig',
@@ -9,10 +10,12 @@ return {
       'LazyGitFilterCurrentFile',
     },
 
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
     keys = {
-      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'Launch [L]azy[G]it' },
     },
+
+    config = function()
+      vim.g.lazygit_floating_window_scaling_factor = 0.5
+    end,
   },
 }
