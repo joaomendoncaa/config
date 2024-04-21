22afaee5b43b664f2b28120169f6bcf2f8951f51
joaomendoncaa <joaomendoncadev@gmail.com>
2024-04-21 11:07:25 +0100

    feat: oil-git-status

--- dotfiles/nvim/lua/custom/plugins/oil.lua
+++ dotfiles/nvim/lua/custom/plugins/oil.lua
@@ -9,6 +9,22 @@ return {
         columns = {
           'icon',
         },
+        win_options = {
+          signcolumn = 'yes:2',
+        },
+      }
+    end,
+  },
+  -- Adds the git status for each file on the buffer
+  -- see: https://github.com/refractalize/oil-git-status.nvim
+  {
+    'refractalize/oil-git-status.nvim',
+    dependencies = {
+      'stevearc/oil.nvim',
+    },
+    config = function()
+      require('oil-git-status').setup {
+        show_ignored = true,
       }
     end,
   },
