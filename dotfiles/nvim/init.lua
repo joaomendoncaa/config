c9cc7cf201eb0fe17cf5cb51ac3c06ad4ed60605
joaomendoncaa <joaomendoncadev@gmail.com>
2024-04-24 10:27:38 +0100

    feat: modularity and remove unwanted icons

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -1,38 +1,13 @@
 require 'globals'
 require 'options'
 require 'keymaps'
+require 'lazyvim'
 
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
----@diagnostic disable-next-line: undefined-field
-vim.opt.rtp:prepend(lazypath)
-
-require('lazy').setup({
+require('lazy').setup {
   'tpope/vim-sleuth',
   'numToStr/Comment.nvim',
   { import = 'custom.plugins' },
-}, {
-  ui = {
-    -- If you have a Nerd Font, set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons otherwise define a unicode icons table
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
-    },
-  },
-})
+}
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
---
+++ dotfiles/nvim/lua/lazyvim.lua
@@ -0,0 +1,5 @@
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
