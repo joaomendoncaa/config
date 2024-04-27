f4b4d3af82dc398205c55b6e96786907b963d985
joaomendoncaa <joaomendoncadev@gmail.com>
2024-04-27 14:49:52 +0100

    fix: treesitter ensure_installed tsx and go

--- dotfiles/nvim/lua/custom/plugins/treesitter.lua
+++ dotfiles/nvim/lua/custom/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
+      ensure_installed = { 'tsx', 'typescript', 'go', 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
