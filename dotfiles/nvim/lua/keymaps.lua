a3e1545b60b4b598dd476bf0bca454c739abe93d
joaomendoncaa <joaomendoncadev@gmail.com>
2024-04-27 14:30:59 +0100

    feat: vim.keymap on keymaps

--- dotfiles/nvim/lua/keymaps.lua
+++ dotfiles/nvim/lua/keymaps.lua
@@ -1,10 +1,12 @@
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+local key = vim.keymap
+
+key.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+key.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+key.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+key.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+key.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -12,16 +14,16 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+key.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- Disable arrow keys in normal mode
-vim.keymap.set('', '<left>', '<cmd>echo "USE H TO MOVE!!"<CR>')
-vim.keymap.set('', '<right>', '<cmd>echo "USE L TO MOVE!!"<CR>')
-vim.keymap.set('', '<up>', '<cmd>echo "USE K TO MOVE!!"<CR>')
-vim.keymap.set('', '<down>', '<cmd>echo "USE J TO MOVE!!"<CR>')
+key.set('', '<left>', '<cmd>echo "USE H TO MOVE!!"<CR>')
+key.set('', '<right>', '<cmd>echo "USE L TO MOVE!!"<CR>')
+key.set('', '<up>', '<cmd>echo "USE K TO MOVE!!"<CR>')
+key.set('', '<down>', '<cmd>echo "USE J TO MOVE!!"<CR>')
 
 -- Oil.nvim
-vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+key.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
 
 -- Open telescope find_files if no arguments are passed to nvim
 vim.api.nvim_create_autocmd('VimEnter', {
@@ -44,5 +46,5 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- fix all the trouble with persisting the yanked text after pasting and not yanking when deleting
-vim.keymap.set({ 'n', 'v' }, 'd', [["_d]], { desc = '[D]elete to null buffer' })
-vim.keymap.set('x', 'p', [["_dP]], { desc = '[P]aste and preserve buffer' })
+key.set({ 'n', 'v' }, 'd', [["_d]], { desc = '[D]elete to null buffer' })
+key.set('x', 'p', [["_dP]], { desc = '[P]aste and preserve buffer' })
