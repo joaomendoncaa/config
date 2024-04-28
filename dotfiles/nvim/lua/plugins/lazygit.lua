e8e40e9830c241b0e440df81449a9de512656c42
joaomendoncaa <joaomendoncadev@gmail.com>
2024-04-29 00:18:13 +0100

    fix: lazygit integration tweaks

--- dotfiles/nvim/lua/plugins/lazygit.lua
+++ dotfiles/nvim/lua/plugins/lazygit.lua
@@ -15,10 +15,10 @@ return {
     },
 
     config = function()
-      vim.g.lazygit_floating_window_scaling_factor = 0.5
-      vim.g.lazygit_floating_window_use_plenary = 1
-      -- vim.g.lazygit_floating_window_border_chars = {'╭','─', '╮', '│', '╯','─', '╰', '│'}
+      vim.g.lazygit_floating_window_scaling_factor = 0.75
+      vim.g.lazygit_floating_window_use_plenary = 0
       vim.g.lazygit_floating_window_border_chars = { '', '', '', '', '', '', '', '' }
+      vim.g.lazygit_floating_window_winblend = 0
     end,
   },
 }
