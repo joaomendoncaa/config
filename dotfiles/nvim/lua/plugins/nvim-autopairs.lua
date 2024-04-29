f90ecc2aea25bb106beb82c992a992f92a3d2b41
joaomendoncaa <joaomendoncadev@gmail.com>
2024-04-29 14:37:23 +0100

    feat: auto '(' after selecting cmp method

--- dotfiles/nvim/lua/plugins/nvim-autopairs.lua
+++ dotfiles/nvim/lua/plugins/nvim-autopairs.lua
@@ -1,5 +1,11 @@
 return {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
-  config = true,
+  config = function()
+    require('nvim-autopairs').setup {}
+    -- automatically add `(` after selecting a function or method
+    local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+    local cmp = require 'cmp'
+    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+  end,
 }
