db97dda03d2580aed3293449f17e97ea590c1c30
joaomendoncaa <joaomendoncadev@gmail.com>
2024-05-10 09:27:49 +0100

    feat: todo comments fzf with telescope

--- dotfiles/nvim/lua/plugins/todo-comments.lua
+++ dotfiles/nvim/lua/plugins/todo-comments.lua
@@ -1,6 +1,18 @@
 return {
   'folke/todo-comments.nvim',
   event = 'VimEnter',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  opts = { signs = false },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope.nvim',
+  },
+  config = function()
+    require('todo-comments').setup {
+      signs = true,
+      keywords = {
+        NOTE = { alt = { 'INFO', 'SEE' } },
+      },
+    }
+
+    vim.keymap.set('n', '<leader>st', '<CMD>TodoTelescope<CR>', { desc = '[S]earch all [T]odos in workspace.' })
+  end,
 }
