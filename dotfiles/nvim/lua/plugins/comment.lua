08475a42015204786eb8b00c83c7935a986a360f
joaomendoncaa <joaomendoncadev@gmail.com>
2024-05-11 13:10:41 +0100

    fix: nvim-ts-context-commentstring missing prehook

--- dotfiles/nvim/lua/plugins/comment.lua
+++ dotfiles/nvim/lua/plugins/comment.lua
@@ -1,16 +1,27 @@
 return {
-  {
-    'numToStr/Comment.nvim',
-    config = function()
-      require('Comment').setup {
-        pre_hook = function()
-          return vim.bo.commentstring
-        end,
-      }
-    end,
-  },
-  {
+  'numToStr/Comment.nvim',
+  event = 'BufReadPre',
+  lazy = false,
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
     'JoosepAlviste/nvim-ts-context-commentstring',
-    config = true,
   },
+  config = function()
+    local prehook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook()
+
+    require('Comment').setup {
+      padding = true,
+      sticky = true,
+      ignore = '^$',
+      toggler = { line = 'gcc', block = 'gbc' },
+      opleader = {
+        line = 'gc',
+        block = 'gb',
+      },
+      extra = { above = 'gcO', below = 'gco', eol = 'gcA' },
+      mappings = { basic = true, extra = true, extended = false },
+      pre_hook = prehook,
+      post_hook = function() end,
+    }
+  end,
 }
