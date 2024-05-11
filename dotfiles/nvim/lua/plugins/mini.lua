cf80bf889cf23f7aabb38874d2b3245c1d89b4fc
joaomendoncaa <joaomendoncadev@gmail.com>
2024-05-11 11:05:00 +0100

    feat: mini.jump2d (game changer)

--- dotfiles/nvim/lua/plugins/mini.lua
+++ dotfiles/nvim/lua/plugins/mini.lua
@@ -4,19 +4,23 @@ return {
     -- see: https://github.com/echasnovski/mini.nvim
     'echasnovski/mini.nvim',
     config = function()
-      -- Better Around/Inside textobjects
+      -- better around/inside textobjects
       --
       --  - va)  - [V]isually select [A]round [)]paren
       --  - yinq - [Y]ank [I]nside [N]ext [']quote
       --  - ci'  - [C]hange [I]nside [']quote
       require('mini.ai').setup { n_lines = 500 }
 
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
+      -- add/delete/replace surroundings (brackets, quotes, etc.)
       --
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Pareninit lua
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
+
+      -- jump interatively with labels
+      -- SEE: https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-jump2d.md
+      require('mini.jump2d').setup()
     end,
   },
 }
