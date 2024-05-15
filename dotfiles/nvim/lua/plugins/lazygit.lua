2352bfd931df365be9b3a0bc91e7efc2a92be18a
joaomendoncaa <joaomendoncadev@gmail.com>
2024-05-15 18:07:08 +0100

    fix: lazygit keymap registered on config instead of init

--- dotfiles/nvim/lua/plugins/lazygit.lua
+++ dotfiles/nvim/lua/plugins/lazygit.lua
@@ -11,12 +11,14 @@ return {
     'LazyGitFilterCurrentFile',
   },
 
+  init = function()
+    vim.keymap.set('n', '<leader>lg', '<CMD>LazyGit<CR>', { desc = 'Launch [L]azy[G]it.' })
+  end,
+
   config = function()
     vim.g.lazygit_floating_window_scaling_factor = 0.75
     vim.g.lazygit_floating_window_use_plenary = 0
     vim.g.lazygit_floating_window_border_chars = { '', '', '', '', '', '', '', '' }
     vim.g.lazygit_floating_window_winblend = 0
-
-    vim.keymap.set('n', '<leader>lg', '<CMD>LazyGit<CR>', { desc = 'Launch [L]azy[G]it.' })
   end,
 }
