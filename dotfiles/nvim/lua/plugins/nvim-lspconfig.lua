297a725f63383a47492074a4c72fea3196e9247e
joaomendoncaa <joaomendoncadev@gmail.com>
2024-05-15 01:51:05 +0100

    feat: inc lsp rename!

--- dotfiles/nvim/lua/plugins/nvim-lspconfig.lua
+++ dotfiles/nvim/lua/plugins/nvim-lspconfig.lua
@@ -62,10 +62,6 @@ return {
           --  Similar to document symbols, except searches over your whole project.
           map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols.')
 
-          -- Rename the variable under your cursor
-          --  Most Language Servers support renaming across files, etc.
-          map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame.')
-
           map('<leader>ca', vim.lsp.buf.code_action, 'List [C]ode [A]ctions.')
 
           -- Opens a popup that displays documentation about the word under your cursor
