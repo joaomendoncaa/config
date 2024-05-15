1897453063de5f35ad514ef9b9e93a9600889da5
joaomendoncaa <joaomendoncadev@gmail.com>
2024-05-15 18:11:38 +0100

    fix: wrong lazy loading of nvim-markdown

--- dotfiles/nvim/lua/plugins/markdown.lua
+++ dotfiles/nvim/lua/plugins/markdown.lua
@@ -4,7 +4,7 @@ return {
     -- SEE: https://github.com/ixru/nvim-markdown
     'ixru/nvim-markdown',
 
-    lazy = true,
+    ft = { 'markdown' },
 
     config = function() end,
   },
@@ -14,7 +14,6 @@ return {
     -- SEE: https://github.com/iamcco/markdown-preview.nvim
     'iamcco/markdown-preview.nvim',
 
-    lazy = true,
     cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
     ft = { 'markdown' },
 
