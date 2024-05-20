8e46c3a4905330f5a14919dc14b058ce0216e2d0
joaomendoncaa <joaomendoncadev@gmail.com>
2024-05-20 10:45:21 +0100

    fix: more confortable

--- dotfiles/nvim/lua/core/keymaps.lua
+++ dotfiles/nvim/lua/core/keymaps.lua
@@ -7,7 +7,7 @@ keymap('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = '[E]scape from search highl
 
 keymap('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message.' })
 keymap('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message.' })
-keymap('n', '<leader>Q', vim.diagnostic.open_float, { desc = '[Q]uickly show diagnostic error messages.' })
-keymap('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list.' })
+keymap('n', '<leader>q', vim.diagnostic.open_float, { desc = '[Q]uickly show diagnostic error messages.' })
+keymap('n', '<leader>Q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list.' })
 
 keymap('x', 'p', [["_dP]], { desc = '[P]aste but preserve the clipboard buffer.' })
