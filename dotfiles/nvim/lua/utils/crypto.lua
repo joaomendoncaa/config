c5a370ba087fce34eb8fe817fbcf6a915f36c367
joaomendoncaa <joaomendoncadev@gmail.com>
2024-06-18 15:29:23 +0100

    feat(crypto): module with hashString function

---
+++ dotfiles/nvim/lua/utils/crypto.lua
@@ -0,0 +1,18 @@
+local M = {}
+
+---
+-- Computes a hash value for the given string.
+--
+---@param str string The input string to hash.
+---@return number The computed hash value.
+function M.hashString(str)
+    local hash = 0
+
+    for i = 1, #str do
+        hash = (hash * 31 + string.byte(str, i)) % 2 ^ 32
+    end
+
+    return hash
+end
+
+return M
