14d941690422187ba9c32ce85c51a99619001aeb
joaomendoncaa <joaomendoncadev@gmail.com>
2024-06-18 15:28:56 +0100

    fix(flags): missing annotations

--- dotfiles/nvim/lua/utils/flags.lua
+++ dotfiles/nvim/lua/utils/flags.lua
@@ -1,5 +1,9 @@
 local M = {}
 
+-- Checks if the given value is 1
+--
+---@param ref any The value to check.
+---@return boolean True if the value is 1, false otherwise.
 function M.isOne(ref)
     if tonumber(ref or 0) ~= 1 then
         return false
