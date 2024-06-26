135fa07695699148810ed0f42f657523bab69e6d
joaomendoncaa <joaomendoncadev@gmail.com>
2024-06-26 21:43:15 +0100

    fix(core): autocmds now need to be inside a table, auto_colorscheme gc

--- dotfiles/nvim/lua/core/options.lua
+++ dotfiles/nvim/lua/core/options.lua
@@ -34,7 +34,9 @@ local auto_greeter = function()
 end
 
 local auto_colorscheme = function(args)
-    require('utils.themes').update(args.match)
+    if args.match then
+        require('utils.themes').update(args.match)
+    end
 end
 
 -- flag plugins that there's a nerd font installed
@@ -42,6 +44,7 @@ g.NVIM_NERD_FONT = true
 
 g.mapleader = ' '
 g.maplocalleader = ' '
+
 o.termguicolors = true
 o.laststatus = 0
 o.signcolumn = 'yes'
@@ -69,19 +72,19 @@ o.whichwrap = 'lh'
 o.wrap = false
 o.conceallevel = 0
 
-autocmd('TextYankPost', {
+autocmd({ 'TextYankPost' }, {
     desc = 'Briefly highlight text range when yanking.',
     group = vim.api.nvim_create_augroup('text-yank-post-highlighting', { clear = true }),
     callback = auto_highlight_yank,
 })
 
-autocmd('ColorScheme', {
+autocmd({ 'ColorScheme' }, {
     desc = 'Make necessary adjustments to the selected colorscheme.',
     group = vim.api.nvim_create_augroup('color-scheme-background-removal', { clear = true }),
     callback = auto_colorscheme,
 })
 
-autocmd('User', {
+autocmd({ 'User' }, {
     desc = 'Echo lazy stats into command line on start.',
     pattern = 'LazyVimStarted',
     callback = auto_greeter,
