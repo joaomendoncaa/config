bb55f23ea23a32db19d977357b4b4d15d779ca4c
joaomendoncaa <joaomendoncadev@gmail.com>
2024-07-01 13:11:53 +0100

    feat(core): move up/down in wrapped lines with j and k

--- dotfiles/nvim/lua/core/keymaps.lua
+++ dotfiles/nvim/lua/core/keymaps.lua
@@ -11,3 +11,6 @@ keymap('n', '<leader>q', vim.diagnostic.open_float, { desc = '[Q]uickly show dia
 keymap('n', '<leader>Q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list.' })
 
 keymap('x', 'p', [["_dP]], { desc = '[P]aste but preserve the clipboard buffer.' })
+
+keymap('n', 'j', 'gj', { desc = '[J] Move down even in wrapped lines.' })
+keymap('n', 'k', 'gk', { desc = '[K] Move up even in wrapped lines.' })
