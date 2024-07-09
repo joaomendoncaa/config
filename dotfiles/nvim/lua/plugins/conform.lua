381836cdae153a0b46ea40dbcb827c2eda322f4e
joaomendoncaa <joaomendoncadev@gmail.com>
2024-07-09 19:36:11 +0100

    typo(conform): disabled_filetypes

--- dotfiles/nvim/lua/plugins/conform.lua
+++ dotfiles/nvim/lua/plugins/conform.lua
@@ -32,11 +32,11 @@ return {
         plugin.setup {
             notify_on_error = false,
             format_on_save = function(bufnr)
-                local disable_filetypes = { c = true, cpp = true }
+                local disabled_filetypes = { c = true, cpp = true }
 
                 return {
                     timeout_ms = 500,
-                    lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+                    lsp_fallback = not disabled_filetypes[vim.bo[bufnr].filetype],
                 }
             end,
             formatters_by_ft = {
