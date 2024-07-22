9012fe3989e8fad944db2983638486d45c681d3e
joaomendoncaa <joaomendoncadev@gmail.com>
2024-07-22 11:58:37 +0100

    fix(neovim): missing types on utils/commands

--- dotfiles/nvim/lua/utils/commands.lua
+++ dotfiles/nvim/lua/utils/commands.lua
@@ -1,15 +1,22 @@
 local M = {}
 
+---@param name string
+---@param command any
+---@param opts? vim.api.keyset.user_command
 function M.user(name, command, opts)
     opts = opts or {}
     vim.api.nvim_create_user_command(name, command, opts)
 end
 
+---@param event any
+---@param opts? vim.api.keyset.create_autocmd
 function M.auto(event, opts)
     opts = opts or {}
     vim.api.nvim_create_autocmd(event, opts)
 end
 
+---@param id string
+---@param opts? vim.api.keyset.create_augroup
 function M.augroup(id, opts)
     opts = opts or {}
     opts.clear = opts.clear or true
