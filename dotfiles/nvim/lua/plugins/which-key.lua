9ea76c9df119841344a5a1ce952258ea0ad84b63
joaomendoncaa <joaomendoncadev@gmail.com>
2024-08-30 13:47:53 +0100

    fix(neovim): whichkey nerdfont support

--- dotfiles/nvim/lua/plugins/which-key.lua
+++ dotfiles/nvim/lua/plugins/which-key.lua
@@ -7,6 +7,43 @@ return {
 
     config = function()
         require('which-key').setup {
+            icons = {
+                -- if there's a nerd font available, set icons to it
+                mappings = vim.g.NVIM_NERD_FONT,
+
+                -- if there's a nerd font, set icons.keys to an empty table
+                -- otherwise, define a table of strings to use as the keys
+                keys = vim.g.NVIM_NERD_FONT and {} or {
+                    Up = '<Up> ',
+                    Down = '<Down> ',
+                    Left = '<Left> ',
+                    Right = '<Right> ',
+                    C = '<C-…> ',
+                    M = '<M-…> ',
+                    D = '<D-…> ',
+                    S = '<S-…> ',
+                    CR = '<CR> ',
+                    Esc = '<Esc> ',
+                    ScrollWheelDown = '<ScrollWheelDown> ',
+                    ScrollWheelUp = '<ScrollWheelUp> ',
+                    NL = '<NL> ',
+                    BS = '<BS> ',
+                    Space = '<Space> ',
+                    Tab = '<Tab> ',
+                    F1 = '<F1>',
+                    F2 = '<F2>',
+                    F3 = '<F3>',
+                    F4 = '<F4>',
+                    F5 = '<F5>',
+                    F6 = '<F6>',
+                    F7 = '<F7>',
+                    F8 = '<F8>',
+                    F9 = '<F9>',
+                    F10 = '<F10>',
+                    F11 = '<F11>',
+                    F12 = '<F12>',
+                },
+            },
             plugins = {
                 registers = true,
 
