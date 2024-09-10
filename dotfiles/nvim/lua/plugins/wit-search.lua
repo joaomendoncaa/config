93dc73fef31b33407775aef4dd4d335000c1c647
joaomendoncaa <joaomendoncadev@gmail.com>
2024-09-10 14:38:10 +0100

    feat: wit-search

---
+++ dotfiles/nvim/lua/plugins/wit-search.lua
@@ -0,0 +1,16 @@
+return {
+    -- Plugin that allows you to perform web searches directly from within neovim!
+    -- SEE: https://github.com/aliqyan-21/wit.nvim
+    'joaomendoncaa/wit.nvim',
+
+    event = 'VeryLazy',
+
+    config = function()
+        require('wit').setup {
+            engine = 'google',
+            command_search = 'Search',
+            command_search_wiki = 'SearchWiki',
+            command_search_visual = 'SearchVisual',
+        }
+    end,
+}
