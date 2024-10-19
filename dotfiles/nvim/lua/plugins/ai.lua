c3bdd8995cccd8553df832a3b917ff0a60b312b0
joaomendoncaa <joaomendoncadev@gmail.com>
2024-10-19 14:10:24 +0100

    feat!(ai): removed local setup

--- dotfiles/nvim/lua/plugins/ai.lua
+++ dotfiles/nvim/lua/plugins/ai.lua
@@ -56,41 +56,7 @@ return {
         config = function()
             local plugin = require 'avante'
 
-            plugin.setup {
-                claude = {
-                    ['local'] = true,
-                },
-                provider = 'ollama',
-                vendors = {
-                    provider = 'ollama',
-                    ---@type AvanteProvider
-                    ollama = {
-                        ['local'] = true,
-                        endpoint = 'http://127.0.0.1:11434/v1',
-                        model = 'codegemma',
-
-                        parse_curl_args = function(opts, code_opts)
-                            return {
-                                url = opts.endpoint .. '/chat/completions',
-                                headers = {
-                                    ['Accept'] = 'application/json',
-                                    ['Content-Type'] = 'application/json',
-                                },
-                                body = {
-                                    model = opts.model,
-                                    messages = require('avante.providers').copilot.parse_message(code_opts),
-                                    max_tokens = 2048,
-                                    stream = true,
-                                },
-                            }
-                        end,
-
-                        parse_response_data = function(data_stream, event_state, opts)
-                            require('avante.providers').openai.parse_response(data_stream, event_state, opts)
-                        end,
-                    },
-                },
-            }
+            plugin.setup {}
         end,
     },
     {
