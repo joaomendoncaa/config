ca4f05d55763550f12b5740945b5e09e21252c14
joaomendoncaa <joaomendoncadev@gmail.com>
2024-11-01 18:14:52 +0000

    chore(blink): remove deprecated stuff

--- dotfiles/nvim/lua/plugins/blink.lua
+++ dotfiles/nvim/lua/plugins/blink.lua
@@ -21,19 +21,10 @@ return {
         ---@module 'blink.cmp'
         ---@type blink.cmp.Config
         plugin.setup {
-            keymap = {
-                hide = '<C-e>',
-                accept = '<C-y>',
-                select_prev = { '<Up>', '<C-p>' },
-                select_next = { '<Down>', '<C-n>' },
-            },
-
             highlight = {
-                -- sets the fallback highlight groups to nvim-cmp's highlight groups
-                -- useful for when your theme doesn't support blink.cmp
-                -- will be removed in a future release, assuming themes add support
                 use_nvim_cmp_as_default = true,
             },
+
             -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
             -- adjusts spacing to ensure icons are aligned
             nerd_font_variant = vim.g.NVIM_NERD_FONT and 'mono' or 'normal',
