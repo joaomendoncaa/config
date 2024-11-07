a6aa59c0f8f0fe34bcba35bab8ee817959d556f7
joaomendoncaa <joaomendoncadev@gmail.com>
2024-11-07 23:02:01 +0000

    feat(sessions): actually working...

--- dotfiles/nvim/lua/plugins/mini.lua
+++ dotfiles/nvim/lua/plugins/mini.lua
@@ -1,24 +1,57 @@
 return {
-    -- Library of 35+ independent Lua modules improving overall Neovim (version 0.7 and higher) experience with minimal effort.
-    -- SEE: https://github.com/echasnovski/mini.nvim
-    'echasnovski/mini.nvim',
+    {
+        -- Library of 35+ independent Lua modules improving overall Neovim (version 0.7 and higher) experience with minimal effort.
+        -- SEE: https://github.com/echasnovski/mini.nvim
+        'echasnovski/mini.nvim',
 
-    event = 'VeryLazy',
+        config = function()
+            local ai = require 'mini.ai'
+            local surround = require 'mini.surround'
+            local sessions = require 'mini.sessions'
+            local move = require 'mini.move'
+            local jump2d = require 'mini.jump2d'
+            local commands = require 'utils.commands'
 
-    config = function()
-        require('mini.ai').setup { n_lines = 500 }
+            local function get_session_path()
+                return vim.fn.getcwd():gsub('[/\\]', '_') .. '.vim'
+            end
 
-        require('mini.surround').setup()
+            commands.auto('VimEnter', {
+                callback = function()
+                    local path = get_session_path()
+                    if vim.fn.filereadable(sessions.config.directory .. path) == 1 then
+                        vim.schedule(function()
+                            sessions.read(path)
+                        end)
+                    end
+                end,
+            })
 
-        require('mini.sessions').setup()
+            commands.auto('VimLeave', {
+                callback = function()
+                    sessions.write(get_session_path())
+                end,
+            })
 
-        require('mini.move').setup()
+            sessions.setup {
+                autoread = false,
+                autowrite = false,
+                directory = vim.fn.stdpath 'data' .. '/sessions/',
+                verbose = { read = false, write = false, delete = false },
+            }
 
-        require('mini.jump2d').setup {
-            mappings = {
-                start_jumping = 'S',
-            },
-            silent = true,
-        }
-    end,
+            ai.setup { n_lines = 500 }
+
+            surround.setup {}
+
+            move.setup {}
+
+            jump2d.setup {
+                mappings = {
+                    start_jumping = 'S',
+                },
+                silent = true,
+            }
+        end,
+    },
 }
