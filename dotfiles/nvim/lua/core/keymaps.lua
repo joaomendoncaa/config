b9524a4d62e5230b4bd85edddc1d50be5f59be2d
joaomendoncaa <joaomendoncadev@gmail.com>
2024-12-14 18:25:08 +0000

    fix: resizing finally

--- dotfiles/nvim/lua/core/keymaps.lua
+++ dotfiles/nvim/lua/core/keymaps.lua
@@ -1,17 +1,45 @@
 -- most keymaps are directly in their related plugin tables
 -- this file is for "global" keymaps unrelated to any plugin
 
-local keymap = vim.keymap.set
+local key = vim.keymap.set
 
-keymap('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = '[E]scape from search highlights.' })
+key('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = '[E]scape from search highlights.' })
 
-keymap('n', '<leader>q', vim.diagnostic.open_float, { desc = '[Q]uickly show diagnostic error messages.' })
-keymap('n', '<leader>Q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list.' })
+key('n', '<leader>q', vim.diagnostic.open_float, { desc = '[Q]uickly show diagnostic error messages.' })
+key('n', '<leader>Q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list.' })
 
-keymap('x', 'p', [["_dP]], { desc = '[P]aste but preserve the clipboard buffer.' })
+key('x', 'p', [["_dP]], { desc = '[P]aste but preserve the clipboard buffer.' })
 
-keymap('n', 'j', 'gj', { desc = '[J] Move down even in wrapped lines.' })
-keymap('n', 'k', 'gk', { desc = '[K] Move up even in wrapped lines.' })
+key('n', 'j', 'gj', { desc = '[J] Move down even in wrapped lines.' })
+key('n', 'k', 'gk', { desc = '[K] Move up even in wrapped lines.' })
 
-keymap({ 'n', 'i', 'v' }, '<C-s>', '<CMD>w<CR>', { desc = '[S]ave the current file.' })
-keymap({ 'n', 'i', 'v' }, '<C-q>', '<CMD>wqa<CR>', { desc = 'Save all files and [Q]uit.' })
+key({ 'n', 'i', 'v' }, '<C-s>', '<CMD>w<CR>', { desc = '[S]ave the current file.' })
+key({ 'n', 'i', 'v' }, '<C-q>', '<CMD>wqa<CR>', { desc = 'Save all files and [Q]uit.' })
+
+key('n', '<C-M-h>', function()
+    if vim.fn.winnr '$' > 1 then
+        return ':vertical resize -3<CR>'
+    end
+    return vim.fn.system 'tmux resize-pane -L 3'
+end, { silent = true, expr = true, desc = 'Smart resize left' })
+
+key('n', '<C-M-l>', function()
+    if vim.fn.winnr '$' > 1 then
+        return ':vertical resize +3<CR>'
+    end
+    return vim.fn.system 'tmux resize-pane -R 3'
+end, { silent = true, expr = true, desc = 'Smart resize right' })
+
+key('n', '<C-M-k>', function()
+    if vim.fn.winnr '$' > 1 then
+        return ':resize -3<CR>'
+    end
+    return vim.fn.system 'tmux resize-pane -U 3'
+end, { silent = true, expr = true, desc = 'Smart resize up' })
+
+key('n', '<C-M-j>', function()
+    if vim.fn.winnr '$' > 1 then
+        return ':resize +3<CR>'
+    end
+    return vim.fn.system 'tmux resize-pane -D 3'
+end, { silent = true, expr = true, desc = 'Smart resize down' })
