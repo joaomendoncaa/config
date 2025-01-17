db14608b5a2656994c11892532b38e28070a20e7
joaomendoncaa <joaomendoncadev@gmail.com>
2025-01-17 16:35:37 +0000

    feat: working async formatter...
    fix: being able to write out of the buffer context
    
    lazyness

--- dotfiles/nvim/lua/plugins/conform.lua
+++ dotfiles/nvim/lua/plugins/conform.lua
@@ -1,59 +1,68 @@
-local commands = require 'utils.commands'
-local strings = require 'utils.strings'
-
-local has_format_on_save = true
-
 return {
     -- Lightweight yet powerful formatter plugin for Neovim.
     -- SEE: https://github.com/stevearc/conform.nvim
     'stevearc/conform.nvim',
 
-    event = { 'BufWritePre' },
+    lazy = true,
+    event = { 'VeryLazy' },
     cmd = { 'Conform', 'ConformInfo' },
     keys = { '<leader>f' },
 
     init = function()
+        local commands = require 'utils.commands'
+        local strings = require 'utils.strings'
         local plugin = require 'conform'
+        local f = require('utils.functions').f
+
+        local has_format_on_save = true
 
         local keymap = vim.keymap.set
 
-        local function format_disable()
-            has_format_on_save = false
-            vim.api.nvim_set_hl(0, 'T', { fg = '#ff0000' })
-            strings.echo(strings.truncateChunks {
-                { '[OFF]', 'T' },
-                { ' ' },
-                { 'Formatting' },
-            })
+        local function set_format_on_save(value, opts)
+            opts = opts or {
+                silent = false,
+            }
+
+            has_format_on_save = value
+
+            if not opts.silent then
+                local hl = value and '#00ff00' or '#ff0000'
+                local text = value and '[ON]' or '[OFF]'
+
+                vim.api.nvim_set_hl(0, 'HL', { fg = hl })
+                strings.echo(strings.truncateChunks {
+                    { text, 'HL' },
+                    { ' ' },
+                    { 'Formatting' },
+                })
+            end
         end
 
-        local function format_enable()
-            has_format_on_save = true
-            vim.api.nvim_set_hl(0, 'T', { fg = '#00ff00' })
-            strings.echo(strings.truncateChunks {
-                { '[ON]', 'T' },
-                { ' ' },
-                { 'Formatting' },
-            })
+        local function format_async(cb)
+            plugin.format({ async = true }, cb or nil)
         end
 
-        local function format()
-            if not has_format_on_save then
-                return
+        local function handle_buf_write(ctx)
+            local write_without_context = function()
+                vim.api.nvim_buf_call(ctx.buf, f(vim.cmd, 'noautocmd write!'))
             end
 
-            plugin.format { async = true, lsp_format = 'lsp_fallback' }
+            if has_format_on_save then
+                format_async(write_without_context)
+            else
+                write_without_context()
+            end
         end
 
-        keymap('n', '<leader>ff', format, { desc = '[F]ormat buffer.' })
-        keymap('n', '<leader>fk', format_enable, { desc = '[F]ormat enable.' })
-        keymap('n', '<leader>fj', format_disable, { desc = '[F]ormat disable.' })
+        keymap('n', '<leader>ff', format_async, { desc = '[F]ormat buffer.' })
+        keymap('n', '<leader>fk', f(set_format_on_save, true), { desc = '[F]ormat enable.' })
+        keymap('n', '<leader>fj', f(set_format_on_save, false), { desc = '[F]ormat disable.' })
 
-        commands.user('Format', format)
-        commands.user('FormatDisable', format_disable)
-        commands.user('FormatEnable', format_enable)
+        commands.user('Format', format_async)
+        commands.user('FormatDisable', f(set_format_on_save, false))
+        commands.user('FormatEnable', f(set_format_on_save, true))
 
-        commands.auto('BufWritePre', { callback = format })
+        commands.auto('BufWriteCmd', { callback = handle_buf_write })
 
         plugin.setup {
             format_on_save = nil,
@@ -76,6 +85,11 @@ return {
                 lua = { 'stylua' },
             },
             formatters = {
+                stylua = {
+                    condition = function()
+                        return vim.bo.filetype == 'lua'
+                    end,
+                },
                 biome = {
                     condition = function()
                         return vim.fn.filereadable(vim.fn.getcwd() .. '/biome.json') == 1
