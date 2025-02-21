62accfb322687d592ee597e7f2fd0e7a34df765c
joaomendoncaa <joaomendoncadev@gmail.com>
2025-02-21 19:04:59 +0000

    feat: flash

---
+++ dotfiles/nvim/lua/plugins/flash.lua
@@ -0,0 +1,18 @@
+return {
+    'folke/flash.nvim',
+
+    event = 'VeryLazy',
+
+    opts = {},
+
+    keys = {
+        {
+            'S',
+            mode = { 'n', 'x', 'o' },
+            function()
+                require('flash').jump()
+            end,
+            desc = 'Flash',
+        },
+    },
+}
