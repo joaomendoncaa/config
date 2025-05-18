41d0b5f3248bd381ff76aaf5cbccf4c137c5aeaf
joaomendoncaa <joaomendoncadev@gmail.com>
2025-05-18 11:15:20 +0100

    fix: resize step

--- dotfiles/nvim/lua/core/keymaps.lua
+++ dotfiles/nvim/lua/core/keymaps.lua
@@ -1,4 +1,7 @@
+local RESIZE_STEP = 15
+
 local clipboard = require 'utils.clipboard'
+
 local key = require('utils.functions').key
 
 key('n', '<Esc>', '<cmd>nohlsearch<CR>', '[E]scape from search highlights.')
@@ -20,28 +23,28 @@ key({ 'n', 'i', 'v' }, '<C-q>', '<CMD>wqa<CR>', 'Save all files and [Q]uit')
 
 key('n', '<C-M-h>', function()
     if vim.fn.winnr '$' > 1 then
-        return ':vertical resize -5<CR>'
+        return string.format(':vertical resize -%d<CR>', RESIZE_STEP)
     end
-    return vim.fn.system 'tmux resize-pane -L 5'
+    return vim.fn.system(string.format 'tmux resize-pane -L %d', RESIZE_STEP)
 end, { silent = true, expr = true, desc = 'Smart resize left' })
 
 key('n', '<C-M-l>', function()
     if vim.fn.winnr '$' > 1 then
-        return ':vertical resize +5<CR>'
+        return string.format(':vertical resize +%d<CR>', RESIZE_STEP)
     end
-    return vim.fn.system 'tmux resize-pane -R 5'
+    return vim.fn.system(string.format 'tmux resize-pane -R %d', RESIZE_STEP)
 end, { silent = true, expr = true, desc = 'Smart resize right' })
 
 key('n', '<C-M-k>', function()
     if vim.fn.winnr '$' > 1 then
-        return ':resize -5<CR>'
+        return string.format(':resize -%d<CR>', RESIZE_STEP)
     end
-    return vim.fn.system 'tmux resize-pane -U 5'
+    return vim.fn.system(string.format 'tmux resize-pane -U %d', RESIZE_STEP)
 end, { silent = true, expr = true, desc = 'Smart resize up' })
 
 key('n', '<C-M-j>', function()
     if vim.fn.winnr '$' > 1 then
-        return ':resize +5<CR>'
+        return string.format(':resize +%d<CR>', RESIZE_STEP)
     end
-    return vim.fn.system 'tmux resize-pane -D 5'
+    return vim.fn.system(string.format 'tmux resize-pane -D %d', RESIZE_STEP)
 end, { silent = true, expr = true, desc = 'Smart resize down' })
