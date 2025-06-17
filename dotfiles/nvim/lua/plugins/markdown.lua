7e5d6839baf73dcfc9a00191fad264aeab771064
joaomendoncaa <joaomendoncadev@gmail.com>
2025-06-17 12:04:25 +0100

    fix: typeerror

--- dotfiles/nvim/lua/plugins/markdown.lua
+++ dotfiles/nvim/lua/plugins/markdown.lua
@@ -16,20 +16,21 @@ return {
 
         config = function()
             local plugin = require 'table-nvim'
+            local custom_mappings = {
+                insert_row_up = '<space>tk',
+                insert_row_down = '<space>tj',
+                insert_column_left = '<space>th',
+                insert_column_right = '<space>tl',
+                insert_table = '<A-t>',
+                insert_table_alt = '<A-S-t>',
+            }
 
             vim.keymap.del('i', '<TAB>')
             vim.keymap.del('i', '<S-TAB>')
 
             plugin.setup {
                 padd_column_separators = true,
-                mappings = {
-                    insert_row_up = '<space>tk',
-                    insert_row_down = '<space>tj',
-                    insert_column_left = '<space>th',
-                    insert_column_right = '<space>tl',
-                    insert_table = '<A-t>',
-                    insert_table_alt = '<A-S-t>',
-                },
+                mappings = custom_mappings,
             }
         end,
     },
