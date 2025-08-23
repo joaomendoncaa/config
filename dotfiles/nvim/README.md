604650f0ecd35441ec36a54fb3f24dbee6768464
joaomendoncaa <joaomendoncadev@gmail.com>
2025-08-23 17:39:08 +0100

    fix: % modified statusline

--- dotfiles/nvim/README.md
+++ dotfiles/nvim/README.md
@@ -1,3 +1,1 @@
-# Neovim
-
-My neovim config, based off of [kickstart.nvim](https://github.com/nvim-lua/kickstart.nvim) (3338d39).
+3338d3920620861f8313a2745fd5d2be39f39534
--- dotfiles/nvim/lua/plugins/fff.lua
+++ dotfiles/nvim/lua/plugins/fff.lua
@@ -1,15 +1,16 @@
 return {
     {
         'dmtrKovalenko/fff.nvim',
+        build = 'cargo build --release',
         opts = {},
         keys = {
-            -- {
-            --     '<leader>ss',
-            --     function()
-            --         require('fff').find_files()
-            --     end,
-            --     desc = 'Open file picker',
-            -- },
+            {
+                '<leader>PP',
+                function()
+                    require('fff').find_files()
+                end,
+                desc = 'Open file picker',
+            },
         },
     },
 }
