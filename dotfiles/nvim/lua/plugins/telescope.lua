76fdb94d23296a20d0cbd086c66bdd4760ba75ae
joaomendoncaa <joaomendoncadev@gmail.com>
2025-10-07 07:18:53 +0100

    feat: telescope defaults

--- dotfiles/nvim/lua/plugins/telescope.lua
+++ dotfiles/nvim/lua/plugins/telescope.lua
@@ -8,6 +8,19 @@ return {
 
         config = function()
             require('telescope').setup {
+                defaults = {
+                    color_devicons = true,
+                    sorting_strategy = 'ascending',
+                    borderchars = { '', '', '', '', '', '', '', '' },
+                    path_displays = 'smart',
+                    layout_strategy = 'horizontal',
+                    layout_config = {
+                        height = 100,
+                        width = 400,
+                        prompt_position = 'top',
+                        preview_cutoff = 40,
+                    },
+                },
                 extensions = {
                     ['ui-select'] = {
                         require('telescope.themes').get_dropdown {},
