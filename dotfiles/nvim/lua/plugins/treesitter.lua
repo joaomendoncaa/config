0c8ab53d5e8807b8bddd24a7c84ee044d6daf49a
joaomendoncaa <joaomendoncadev@gmail.com>
2026-04-04 17:19:22 +0100

    fix: nvim-install breaking TS

--- dotfiles/nvim/lua/plugins/treesitter.lua
+++ dotfiles/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,44 @@
 return {
     'nvim-treesitter/nvim-treesitter',
 
-    event = { 'BufReadPost', 'BufNewFile' },
-    cmd = { 'TSInstall', 'TSBufEnable', 'TSBufDisable', 'TSModuleInfo' },
+    lazy = false,
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs',
+
+    init = function()
+        local errors = require 'utils.errors'
+
+        errors.ignore {
+            'treesitter.-Index out of bounds',
+        }
+
+        local ensure_installed = {
+            'tsx',
+            'typescript',
+            'go',
+            'bash',
+            'c',
+            'diff',
+            'html',
+            'lua',
+            'luadoc',
+            'markdown',
+            'markdown_inline',
+            'query',
+            'vim',
+            'vimdoc',
+        }
+
+        require('nvim-treesitter').install(ensure_installed)
+
+        vim.api.nvim_create_autocmd('FileType', {
+            callback = function()
+                pcall(vim.treesitter.start)
+                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end,
+        })
+    end,
 
     config = function()
-        local config = require 'nvim-treesitter.configs'
-        local install = require 'nvim-treesitter.install'
         local errors = require 'utils.errors'
 
         errors.ignore {
@@ -54,63 +84,8 @@ return {
 
         key('n', '<leader>gp', git_plugin, '[G]o [G]it [P]lugin')
 
-        vim.opt.runtimepath:append(vim.fn.stdpath 'data' .. '/treesitter-parser')
         vim.opt.verbose = 0
 
-        install.prefer_git = false
-        install.compilers = { 'gcc', 'cc', 'clang' }
-        install.parser_install_dir = vim.fn.stdpath 'data'
-
-        config.setup {
-            sync_install = false,
-            parser_install_dir = nil,
-            auto_install = true,
-            quiet_install = true,
-            install = {
-                -- suppress installation failure messages
-                on_failure = function() end,
-            },
-            ensure_installed = {
-                'tsx',
-                'typescript',
-                'go',
-                'bash',
-                'c',
-                'diff',
-                'html',
-                'lua',
-                'luadoc',
-                'markdown',
-                'markdown_inline',
-                'query',
-                'vim',
-                'vimdoc',
-            },
-            textobjects = {
-                select = {
-                    enable = false,
-                    keymaps = {
-                        ['af'] = '@function.outer',
-                        ['if'] = '@function.inner',
-                        ['ac'] = '@class.outer',
-                        ['ic'] = '@class.inner',
-                    },
-                },
-            },
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = { 'ruby' },
-
-                disable = function(_, buf)
-                    local max_filesize = 50 * 1024
-
-                    local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
-                    if ok and stats and stats.size > max_filesize then
-                        return true
-                    end
-                end,
-            },
-            indent = { enable = true, disable = { 'ruby' } },
-        }
+        require('nvim-treesitter').setup {}
     end,
 }
