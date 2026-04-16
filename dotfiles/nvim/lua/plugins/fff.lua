907ae627d2d4622e0b7fac29d7afd812a27b91e6
joaomendoncaa <joaomendoncadev@gmail.com>
2026-04-16 19:56:22 +0100

    fix: fkn hypridle

--- dotfiles/nvim/lua/plugins/fff.lua
+++ dotfiles/nvim/lua/plugins/fff.lua
@@ -4,6 +4,7 @@ return {
         opts = {
             prompt = '> ',
             layout = { prompt_position = 'top', max_threads = 11, height = 0.7, width = 0.7 },
+            keymaps = { select_vsplit = '<C-x>' },
         },
         keys = {
             {
