716f5238f01c17af29e0f5490dc3eeb4c5556c43
joaomendoncaa <joaomendoncadev@gmail.com>
2026-05-11 11:43:06 +0100

    feat: nvim-tree out neo-tree in

---
+++ dotfiles/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,31 @@
+return {
+    {
+        'nvim-neo-tree/neo-tree.nvim',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'MunifTanjim/nui.nvim',
+            'nvim-tree/nvim-web-devicons',
+        },
+        keys = {
+            {
+                '<C-e>',
+                function()
+                    vim.cmd('Neotree toggle position=current')
+                end,
+                desc = 'Toggle [E]xplorer',
+            },
+        },
+        config = function()
+            require('neo-tree').setup {
+                default_component_configs = {
+                    container = {
+                        enable_character_fade = false,
+                    },
+                },
+                filesystem = {
+                    follow_current_file = true,
+                },
+            }
+        end,
+    },
+}
