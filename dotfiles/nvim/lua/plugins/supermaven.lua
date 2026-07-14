f714589036847d14f6e5998158352bad887bfa82
joaomendoncaa <joaomendoncadev@gmail.com>
2026-07-14 14:18:24 +0100

    fix: supermaven bs

--- dotfiles/nvim/lua/plugins/supermaven.lua
+++ dotfiles/nvim/lua/plugins/supermaven.lua
@@ -34,6 +34,38 @@ return {
                 keymaps = { clear_suggestion = '<C-c>' },
                 log_level = 'off',
             }
+
+            -- Monkey-patch log:warn/log:error to respect log_level (plugin bug:
+            -- they bypass the level check and always call nvim_notify)
+            local logger = require 'supermaven-nvim.logger'
+            local config = require 'supermaven-nvim.config'
+            local orig_warn = logger.warn
+            local orig_error = logger.error
+            logger.warn = function(self, msg)
+                if config.config.log_level == 'off' then
+                    return
+                end
+                orig_warn(self, msg)
+            end
+            logger.error = function(self, msg)
+                if config.config.log_level == 'off' then
+                    return
+                end
+                orig_error(self, msg)
+            end
+
+            -- Monkey-patch on_update to check buffer size cheaply (nvim_buf_get_offset
+            -- is O(1)) before doing the expensive nvim_buf_get_lines + table.concat
+            local binary = require 'supermaven-nvim.binary.binary_handler'
+            local orig_on_update = binary.on_update
+            binary.on_update = function(self, buffer, file_name, event_type)
+                local line_count = vim.api.nvim_buf_line_count(buffer)
+                local byte_size = line_count > 0 and vim.api.nvim_buf_get_offset(buffer, line_count) or 0
+                if byte_size > self.HARD_SIZE_LIMIT then
+                    return
+                end
+                return orig_on_update(self, buffer, file_name, event_type)
+            end
         end,
     },
 }
