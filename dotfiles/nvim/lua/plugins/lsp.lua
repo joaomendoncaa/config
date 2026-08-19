9d9836821eb9f0b738f87144bf40dd3d7bff111e
joaomendoncaa <joaomendoncadev@gmail.com>
2026-08-19 07:40:38 +0100

    feat: add gdscript LSP over TCP and opencode websearch provider

--- dotfiles/nvim/lua/plugins/lsp.lua
+++ dotfiles/nvim/lua/plugins/lsp.lua
@@ -313,5 +313,15 @@ return {
         require('mason-lspconfig').setup {
             handlers = { handle_mason_setup },
         }
+
+        -- connects to the godot editor's built-in LSP over TCP
+        -- (default port 6005, override with the GDScript_Port env var)
+        -- Requires the editor to be open on the project directory
+        vim.lsp.config('gdscript', {
+            cmd = vim.lsp.rpc.connect('127.0.0.1', tonumber(os.getenv 'GDScript_Port' or '6005')),
+            filetypes = { 'gdscript' },
+            root_markers = { 'project.godot', '.git' },
+        })
+        vim.lsp.enable 'gdscript'
     end,
 }
